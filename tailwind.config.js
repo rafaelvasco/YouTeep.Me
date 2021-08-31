61e8f94688993e8b1cd34c856ce0afc28f596dca
Rafael Vasco <rafaelvasco87@gmail.com>
2021-08-31 14:54:01 -0300

    Alter tailwind purge options

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     purge: {
         content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
         options: {
-            safelist: ['type'], // [type='checkbox']
+            safelist: ['type'],
+            whitelistPatterns: [/.*bg-.*/, /.*border-.*/],
         },
     },
     darkMode: 'class',
